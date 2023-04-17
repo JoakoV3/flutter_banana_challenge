5ed78683b3d209a07ab0b65bde03bb59ef3727a4
Joaquin Varela <58125197+JoakoV3@users.noreply.github.com>
2023-04-17 14:56:51 -0300

    👨🏻‍💻 Init & User Model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+class UserModel {
+  UserModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+    required this.token,
+  });
+
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String image;
+  final String token;
+
+  factory UserModel.fromRawJson(String str) =>
+      UserModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json["id"],
+        username: json["username"],
+        email: json["email"],
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        gender: json["gender"],
+        image: json["image"],
+        token: json["token"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "username": username,
+        "email": email,
+        "firstName": firstName,
+        "lastName": lastName,
+        "gender": gender,
+        "image": image,
+        "token": token,
+      };
+}
